3d5cfcace4eb168f30abca5a3dbaa93d4465d41b
perehelion <inperegelion@gmail.com>
2018-06-02 23:52:18 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+
+function send404(response) {
+    response.writeHead(404, {
+        'Content-Type': 'text/plain'
+    });
+    response.write('Error 404: Resource not found.');
+    response.end();
+}
+
+var mimeLookup = {
+    '.js': 'application/javascript',
+    '.html': 'text/html',
+    '.css':'text/css'
+};
+
+var server = http.createServer(function (req, res) {
+    if (req.method == 'GET') {
+        var fileurl;
+        if (req.url == '/')
+            fileurl = './index.html';
+        else
+            fileurl = req.url;
+        var filepath = path.resolve('./' + fileurl);
+
+        var fileExt = path.extname(filepath);
+        var mimeType = mimeLookup[fileExt];
+        if (!mimeType) {
+            send404(res);
+            return;
+        }
+        fs.exists(filepath, function (exists) {
+            if (!exists) {
+                send404(res);
+                return;
+            };
+            res.writeHead(200, {
+                'content-type': mimeType
+            });
+            fs.createReadStream(filepath).pipe(res);
+        });
+    } else {
+        send404(res);
+    }
+}).listen(8085);
+
+console.log('server running on port 8085');
